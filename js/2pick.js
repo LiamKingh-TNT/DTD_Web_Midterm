5ff2d087914c79d54b83af6c64664b92ca7b0bb7
LiamKingh-TNT <commandblock365@gmail.com>
2024-10-28 23:00:38 +0800

    Update

--- js/2pick.js
+++ js/2pick.js
@@ -1,61 +1,67 @@
-var random;
-$(document).ready(function(){
+var moving = 1;
+
+$(document).ready(function() {
     // 頁面加載後自動播放動畫
     $('#sel_1').addClass('sel_1_pop_up_play');
     $('#sel_2').addClass('sel_2_pop_up_play');
     play_sel_anim();
-    
     $('.pick_selection').hover(
-        function(){
-            $(this).find('.selected_beam').addClass('visible');
-            $(this).find('.selected_beam').removeClass('hide');
+        function() {
+            if(moving == 0)
+            {
+                return null;
+            }
+            $('#sel_1').removeClass('sel_1_pop_up_play sel_1_pop_up_play_v2 selected unselected');
+            $('#sel_2').removeClass('sel_2_pop_up_play sel_2_pop_up_play_v2 selected unselected');
+    
+            $(this).animate({'width' : '35em','height':'35em'}, 300, 'linear',function(){});
+            $('.pick_selection').removeClass('selected');
+            $(this).addClass('selected');
+            $(this).find('.selected_beam').removeClass('hide').addClass('visible');
         },
-        function(){
-            $(this).find('.selected_beam').removeClass('visible');
-            $(this).find('.selected_beam').addClass('hide');
+        function() {
+            if(moving == 0)
+            {
+                return null;
+            }
+            $('#sel_1').removeClass('sel_1_pop_up_play sel_1_pop_up_play_v2 selected unselected');
+            $('#sel_2').removeClass('sel_2_pop_up_play sel_2_pop_up_play_v2 selected unselected');
+            $(this).addClass('selected');
+            $(this).animate({'width' : '30em','height':'30em'}, 300, 'linear',function(){});
+            $(this).find('.selected_beam').removeClass('visible').addClass('hide');
         }
     );
 });
 
-function play_sel_anim()
-{
-    // 點擊事件來重新播放動畫
-    
-    $('.pick_selection').click(function(){
+function play_sel_anim() {
+    $('.pick_selection').click(function() {
+        $('.selected_beam').removeClass('visible').addClass('hide');
+        $(this).animate({'width' : '30em','height':'30em'}, 10, 'linear',function(){});
         $('#test_title').css('color', 'white');
         random = Math.random();
-        if(random < 0.5)
-        {
-            // 移除並重新添加類別來重播動畫
-            $('#sel_1').removeClass('sel_1_pop_up_play');
-            $('#sel_2').removeClass('sel_2_pop_up_play');
-            $('#sel_1').removeClass('sel_1_pop_up_play_v2');
-            $('#sel_2').removeClass('sel_2_pop_up_play_v2');
-    
-            // 強制重繪以使動畫生效
-            void $('#sel_1')[0].offsetWidth;
-            void $('#sel_2')[0].offsetWidth;
-    
-            // 重新添加動畫類別
+        $('.pick_selection').addClass('disable-hover'); // 禁用 hover
+        moving = 0;
+        // 移除並重播動畫
+        $('#sel_1').removeClass('sel_1_pop_up_play sel_1_pop_up_play_v2 selected unselected');
+        $('#sel_2').removeClass('sel_2_pop_up_play sel_2_pop_up_play_v2 selected unselected');
+
+        // 強制重繪以使動畫生效
+        void $('#sel_1')[0].offsetWidth;
+        void $('#sel_2')[0].offsetWidth;
+
+        // 重新添加動畫
+        if (random < 0.5) {
             $('#sel_1').addClass('sel_1_pop_up_play');
             $('#sel_2').addClass('sel_2_pop_up_play');
-        }
-        else
-        {
-            // 移除並重新添加類別來重播動畫
-            $('#sel_1').removeClass('sel_1_pop_up_play_v2');
-            $('#sel_2').removeClass('sel_2_pop_up_play_v2');
-            $('#sel_1').removeClass('sel_1_pop_up_play');
-            $('#sel_2').removeClass('sel_2_pop_up_play');
-    
-            // 強制重繪以使動畫生效
-            void $('#sel_1')[0].offsetWidth;
-            void $('#sel_2')[0].offsetWidth;
-    
-            // 重新添加動畫類別
+        } else {
             $('#sel_1').addClass('sel_1_pop_up_play_v2');
             $('#sel_2').addClass('sel_2_pop_up_play_v2');
-
         }
+
+        // 設定定時器在動畫結束後重新啟用按鈕
+        setTimeout(() => {
+            $('.pick_selection').removeClass('disable-hover'); // 移除 disable-hover class
+            moving = 1;
+        }, 1100); // 700毫秒對應於動畫的時長
     });
 }
