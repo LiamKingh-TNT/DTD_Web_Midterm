3d391ccf145bc1e3d6edbafabc76fdad035ec278
jxex <s111219046@stu.ntue.edu.tw>
2024-10-29 11:58:02 +0800

    update

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 $(document).ready(function() {
     $('button').mouseenter(function(){
+<<<<<<< Updated upstream
         $(this).stop(true,true).animate({'width': '+=25'}, 300, 'linear', function(){});
     });
     $('button').mouseleave(function(){
         $(this).animate({'width': '-=25'}, 300, 'linear', function(){});
+=======
+        $(this).stop(true, true).animate({'width': '+=25px'}, 300, 'linear');
+    });
+    $('button').mouseleave(function(){
+        $(this).stop(true, true).animate({'width': '-=25px'}, 300, 'linear');
+>>>>>>> Stashed changes
     });
 });
