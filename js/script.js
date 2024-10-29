2c17283f251e5c7c57a4d2dea0f59418a4b57882
jxex <s111219046@stu.ntue.edu.tw>
2024-10-29 11:52:45 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,8 @@
 $(document).ready(function() {
     $('button').mouseenter(function(){
-        $(this).stop(true, true).animate({'height': '+=25px'}, 300, 'linear');
+        $(this).stop(true,true).animate({'width': '+=25'}, 300, 'linear', function(){});
     });
     $('button').mouseleave(function(){
-        $(this).stop(true, true).animate({'height': '-=25px'}, 300, 'linear');
-    });
-    $('button').click(function() {
-
-        var img1Src = $(this).find('img:first').attr('src');
-        var img2Src = $(this).find('img:last').attr('src');
-
-
-        $('.left-image').attr('src', img1Src);
-        $('.right-image').attr('src', img2Src);
+        $(this).animate({'width': '-=25'}, 300, 'linear', function(){});
     });
 });
